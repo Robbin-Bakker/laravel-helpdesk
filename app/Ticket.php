ec7cb72a2794adc461a8cf3a8f6f6c137acc1479
Robbin B <rbakker46@st.noorderpoort.nl>
2020-03-11 12:11:40 +0100

    stap :eight:

---
+++ app/Ticket.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+
+    public function submitting_user(){
+        return $this->belongsTo('App/User', 'user_id');
+    }
+    public function assigned_users(){
+        return $this->hasMany('App/User');
+    }
+    public function status(){
+        return $this->belongsTo('App/Status');
+    }
+    public function comments(){
+        return $this->hasMany('App/Comment');
+    }
+    
+}
--- app/User.php
+++ app/User.php
@@ -40,4 +40,10 @@ class User extends Authenticatable
     public function role(){
         return $this->belongsTo('App/Role');
     }
+    public function submitted_tickets(){
+        return $this->hasMany('App/Ticket');
+    }
+    public function assigned_tickets(){
+        return $this->hasMany('App/Ticket');
+    }
 }
