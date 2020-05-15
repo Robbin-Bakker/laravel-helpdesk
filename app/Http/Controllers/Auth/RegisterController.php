b3da42ea132e464d4d3adfb9811834768b97bc64
Robbin Bakker <robbinb.business@gmail.com>
2020-05-15 18:45:03 +0200

    stap 13/14

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,9 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+// custom
+use App\Role;
+
 class RegisterController extends Controller
 {
     /*
@@ -64,10 +67,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $role_id = Role::where('name', Role::CUSTOMER)->value('id');
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role_id' => $role_id,
         ]);
     }
 }
