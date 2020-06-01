8a2fcd8a962000de58068c6120280f5a3344467c
Robbin Bakker <robbinb.business@gmail.com>
2020-06-01 18:23:35 +0200

    stap 21

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        @auth
+                            <!-- menu -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('ticket_index') }}">{{ __('My tickets') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('ticket_create') }}">{{ __('New ticket') }}</a>
+                            </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
